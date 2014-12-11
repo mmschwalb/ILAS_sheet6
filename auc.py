4bd3ef8c00d5af68ab2ab15423550dfc0140b250
Patrick Kaster <kaster@cs.uni-bonn.de>
2014-12-11 20:09:45 +0100

    4. (c) fixed, just randamize new indices for pos. examples, rather than trying to hit permuatation of original S

--- auc.py
+++ auc.py
@@ -7,36 +7,42 @@ from random import shuffle
 if sys.version_info[0] < 3 or sys.version_info[0] == 3 and sys.version_info[1] < 2:
 	raise Exception('Need Python with version newer than 3.2')
 
-def generateExampleSet(setCardinality, nPosExamples):
-	S = [0]*setCardinality
+""" generate an Example set with nPosExamples positive examples and a cardinality of SetCardinality """
+def generateExampleSet(SetCardinality, nPosExamples):
+	S = [0]*SetCardinality
 	for i in range(0,nPosExamples):
-		rindex = randint(0, setCardinality-1)		
+		rindex = randint(0, SetCardinality-1)		
 		while S[rindex] == 1:		
-			rindex = randint(0, setCardinality-1)
+			rindex = randint(0, SetCardinality-1)
 		S[rindex] = 1
 	return S
 
+""" generate a random permutation of the Set S """
 def generateRandomPermutation(S):
 	pi = list(S)
 	shuffle(pi)
 	return pi
 
-def generateRandomPermutationFrontBack(S, m, nPosExamples):
-	pi = generateRandomPermutation(S)
-	if m>0:
-		# not enough positives in range, get new permutation!		
-		while sum(pi[0:m]) < nPosExamples:
-			pi = generateRandomPermutation(S)
-	else:
-		# same, but from the end of pi		
-		while sum(pi[-0:m]) < nPosExamples:
-			pi = generateRandomPermutation(S)
+""" generate a random permutation of the Set S, where number of positive examples nPosExamples are located in the first/last
+m indices, depending on m being positive or negative
+( actually new Example Set is created, prob. for hitting a satisfying random permutation with shuffling is low ) """
+def generateRandomPermutationFrontBack(S, m, nPosExamples):	
+	pi = [0]*len(S)
+	for i in range(0,nPosExamples):
+		rindex = randint(0, abs(m)-1)
+		while pi[rindex] == 1:		
+			rindex = randint(0, abs(m)-1)
+		pi[rindex] = 1
+	if m<0:
+		pi.reverse()
 	return pi
 			
 	
-def calculateAUC(S, setCardinality, nPosExamples):
+""" construct an approximation for ROC from Set S and calculate area under curve by
+summation over deltaX * y[i] """
+def calculateAUC(S, SetCardinality, nPosExamples):
 	# construct approx. for ROC
-	nNegExamples = setCardinality - nPosExamples	
+	nNegExamples = SetCardinality - nPosExamples	
 	values = [[0, 0]]	
 	for example in S:
 		if example == 0:
@@ -51,22 +57,21 @@ def calculateAUC(S, setCardinality, nPosExamples):
 	return area
 
 if __name__ == "__main__":
-	setCardinality = 1000
+	SetCardinality = 1000
 	nPosExamples = 10
-	S = generateExampleSet(setCardinality, nPosExamples)
+	S = generateExampleSet(SetCardinality, nPosExamples)
 	
 	print("\npart (b):")	
 	for i in range(1,11):	
 		pi = generateRandomPermutation(S)
-		area = calculateAUC(pi, setCardinality, nPosExamples)
+		area = calculateAUC(pi, SetCardinality, nPosExamples)
 		print ("area experiment #{0:2}: {1}".format(i, area))
 	
 	print("\npart (c):")
 	M = [500, 100, 50, 25, 20, 15, 10]
 	for m in M:
 		pi = generateRandomPermutationFrontBack(S, m, nPosExamples)
-		areaForward = calculateAUC(pi, setCardinality, nPosExamples)
+		areaForward = calculateAUC(pi, SetCardinality, nPosExamples)
 		pi = generateRandomPermutationFrontBack(S, -m, nPosExamples)
-		areaBackward = calculateAUC(pi, setCardinality, nPosExamples)
+		areaBackward = calculateAUC(pi, SetCardinality, nPosExamples)
 		print ("area experiment pi_m={0:3} (front of pi/back of pi): {1} / {2}".format(m, areaForward, areaBackward))
-
